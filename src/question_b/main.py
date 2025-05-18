fe8ebf5c3b70032e70dfd78f4508f3a3680b587f
vencoop45 <ven.cooper@g.austincc.edu>
2025-05-18 04:02:22 +0000

    Question 2

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,15 @@
 #add import
+
+from src.question_b.question_b import create_multiplication_table, display_multiplication_table
+
+def main():
+    while True:
+        multiplication_table = create_multiplication_table()
+        display_multiplication_table(multiplication_table)
+
+        keep_going = input('Do you want to generate another table? (y/n):').lower()
+        if keep_going != 'y':
+            break
+
+if __name__ == "__main__":
+    main()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,17 @@
 #write functions here, don't add input('') statements here!
+
+def create_multiplication_table():
+    table = []
+    for row_num in range(1, 6):
+        current_row = []
+        for col_num in range(1, 11):
+            value = row_num * col_num
+            current_row.append(value)
+        table.append(current_row)
+    return table
+
+def display_multiplication_table(table_list):
+    for row in table_list:
+        for element in row:
+            print(f'{element}\t', end='')
+        print()
