2fde2968a52d3d59ae4e741d69fca0a153255100
vencoop45 <ven.cooper@g.austincc.edu>
2025-05-18 04:50:47 +0000

    Question 4

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,22 @@
 #add import
+from src.question_d.question_d import stock_purchase_history
+
+def main():
+    while True:
+        print("\nMenu")
+        print("1 - Display stock purchase history")
+        print("2 - Exit")
+
+        try:
+            choice = int(input("Please enter your choice:"))
+            if choice == 1:
+                stock_purchase_history()
+            elif choice == 2:
+                print("Exiting program")
+            else:
+                print("Invalid, please try again.")
+        except ValueError:
+            print("Invalid, please try again.")
+
+if __name__ == '__main__':
+    main()
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,34 @@
 #write functions here, don't add input('') statements here!
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+    def get_symbol(self):
+        return self.__symbol
+    def get_company_name(self):
+        return self.__company_name
+    
+def stock_purchase_history():
+    aapl = Stock("AAPL", "Apple Inc")
+    cat = Stock("CAT", "Caterpillar")
+    ek = Stock("EK", "Eastman Kodak")
+    goog = Stock("GOOG", "Google")
+    msft = Stock("MSFT", "Microsoft")
+
+    stock_dict = {
+        aapl.get_symbol(): aapl,
+        cat.get_symbol(): cat,
+        ek.get_symbol(): ek, 
+        goog.get_symbol(): goog, 
+        msft.get_symbol(): msft
+    }
+
+    print("\nStock Purchase History:")
+    print("-" * 30)
+
+    for symbol in stock_dict:
+        stock_object = stock_dict[symbol]
+        print(f"{stock_object.get_symbol()} - {stock_object.get_company_name()}")
+    print("-" * 30)
+    
