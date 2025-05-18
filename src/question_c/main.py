cb77609aebf3aa1bf7bf6b7ac1e04fb8aaa33a6a
vencoop45 <ven.cooper@g.austincc.edu>
2025-05-18 04:32:57 +0000

    Question 3

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,30 @@
 #add import
+from src.question_c.question_c import get_stock_list, display_stock_list
+
+def main():
+    keep_going = 'y'
+    while keep_going.lower() == 'y':
+        current_stock_list = get_stock_list()
+
+        print('\n----- Stock Menu -----')
+        print('1 - Display stock purchase history')
+        print('2 - Exit')
+        print('---------------')
+
+        try:
+            choice = int(input('Enter your choice:'))
+            while choice < 1 or choice > 2:
+                print('Invalid, please try again.')
+                break
+
+            if choice == 1:
+                display_stock_list(current_stock_list)
+
+            elif choice == 2:
+                keep_going = 'n'
+        except ValueError:
+            print("Invalid input, please try again. ")
+    print("\nExiting program.")
+
+if __name__ == '__main__':
+    main()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,34 @@
 #write functions here, don't add input('') statements here!
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+    def get_symbol(self):
+        return self.__symbol
+    def get_company_name(self):
+        return self.__company_name
+    
+def get_stock_list():
+    stock_list = []
+    stock1 = Stock('AAPL', 'Apple Inc')
+    stock2 = Stock('CAT', 'Caterpillar')
+    stock3 = Stock('EK', 'Eastman Kodak')
+    stock4 = Stock('GOOG', 'Google')
+    stock5 = Stock('MSFT', 'Microsoft')
+
+    stock_list.append(stock1)
+    stock_list.append(stock2)
+    stock_list.append(stock3)
+    stock_list.append(stock4)
+    stock_list.append(stock5)
+
+    return stock_list
+
+def display_stock_list(stocks):
+    print('\nStock Report')
+    print('Company    Symbol')
+    print('-----------------')
+
+    for stock in stocks:
+        print(f'{stock.get_company_name():<15} {stock.get_symbol()}')
